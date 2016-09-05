1c3e8fdf0ba4fb99a35f37e22a1e30601335e29e
slashsorin <sorin.dimo@yahoo.com>
2016-09-05 11:17:11 +0200

    First step, sharing common logic between iOS & Android apps.

---
+++ 2016-fall/slashsorin/CashConvertor/CashConvertor/CurrencyConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace CashConvertor
+{
+	public class CurrencyConverter
+	{
+		string input;
+
+		public CurrencyConverter(string _input)
+		{
+			input = _input;
+		}
+
+		public string ConvertCurrency()
+		{
+			var output = "";
+			if (input.Length < 1)
+			{
+				output = "Please enter a value!";
+			}
+			else
+			{
+				try
+				{
+					var result = Convert.ToDouble(input) * 0.69;
+					output = "$" + input + " = £" + result;
+				}
+				catch (Exception e)
+				{
+					output = "Please enter a number!";
+					Console.WriteLine("Error converting the amount: " + e.Message);
+				}
+			}
+
+			return output;
+		}	
+	}
+}
+
---
+++ 2016-fall/slashsorin/CashConvertor/CashConvertor/MyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CashConvertor
+{
+	public class MyClass
+	{
+		public MyClass()
+		{
+		}
+	}
+}
+
