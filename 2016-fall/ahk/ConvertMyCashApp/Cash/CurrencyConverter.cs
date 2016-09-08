99096f1fbd119a1e58b8988667a9203a1e596649
AndersHqst <ahk@e-conomic.com>
2016-09-08 10:48:01 +0200

    Removed nested .git

---
+++ 2016-fall/ahk/ConvertMyCashApp/Cash/CurrencyConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Cash {
+
+	public class CurrencyConverter {
+
+		static double poundExchangeRate = 0.69;
+
+		/// <exception cref="FormatException">The input cannot be cast to a double</exception>
+		public static string ConvertDollarsToPunds(string input) {
+			double formatInput = Convert.ToDouble(input);
+			double result = formatInput * poundExchangeRate;
+			return result.ToString();
+		}
+	}
+}
+
