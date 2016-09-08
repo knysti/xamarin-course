d39b7994353204b3a079b866f043f8a2635b05d7
AndersHqst <ahk@e-conomic.com>
2016-09-08 10:41:22 +0200

    Added solution to Convert My Cash

---
+++ 2016-fall/ahk/Cash/Cash/StringLib.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Cash
+{
+	public class StringLib
+	{
+		public static string CONVERSION_ERROR_TITLE = "Invalid input";
+		public static string CONVERSION_ERROR_MESSAGE = "Please enter a valid amount in $";
+		public static string OK = "Ok";
+
+		public static string DollarToPunds(string dollars, string punds) {
+			return "$" + dollars + " = £" + punds;
+		}
+	}
+}
+
